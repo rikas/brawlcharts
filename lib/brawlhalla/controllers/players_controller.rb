85a92edec3417d59c2dee7dfb803b012f7a08eac
rikas <oterosantos@gmail.com>
2018-10-29 12:02:34 +0000

    Add Bulma and layout

--- lib/brawlhalla/controllers/players_controller.rb
+++ lib/brawlhalla/controllers/players_controller.rb
@@ -6,21 +6,25 @@ require 'brawlhalla/models'
 module Brawlhalla
   class PlayersController < Sinatra::Application
     set :public_folder, -> { Brawlhalla.assets_dir }
-    set :views, -> { Brawlhalla.views_dir('players') }
+    set :views, -> { Brawlhalla.views_dir }
     enable :logging
     enable :static
     # disable :dump_errors
     # disable :show_exceptions
 
     get '/' do
-      "ROOT #{self.class.root}"
+      @players = Player.all
+      @title = 'Players'
+
+      erb :'players/index', layout: :application
     end
 
     get '/:name' do
       @player = Player.find(name: params[:name])
       @rankings = @player.rankings.reverse
+      @title = "Player #{@player.name}"
 
-      erb :show
+      erb :'players/show', layout: :application
     end
   end
 end
